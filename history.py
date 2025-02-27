1e65ca284a87c48d0fc2230214d9d3ee95b9b7aa
Kaeyoung <Kaeyounglk@163.com>
2025-02-27 16:46:26 +1100

    construct history class and method

--- history.py
+++ history.py
@@ -1,15 +1,26 @@
 class History:
     def __init__(self):
-        self.data = [{"results":"...", "feedback":"..."},{"results":"...", "feedback":"..."},{"results":"...", "feedback":"..."}]
+        self.data = []
 
-    def to_str(self):
-        pass 
+    def store(self, result: str, feedback: str = ''):
+        self.data.append({'result': result, 'feedback': feedback})
 
     def get_history(self, idx=None):
+        if not self.data:
+            return None
 
-        if len(self.data) == 0:
-            return ""
-        if idx==None:
-            ##TODO output all history, e.g., return self_to_str()
-            pass
-        return self.data[idx]
+        if idx is None:
+            return self.data
+        else:
+            if isinstance(idx, int) and 0 <= idx < len(self.data):
+                return self.data[idx]
+            else:
+                return None
+        
+    def get_latest(self):
+        if not self.data:
+            return None
+        return self.data[-1]
+
+    def __str__(self):
+        return f"History={self.data}"
